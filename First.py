ad4f4041e856bb77f9fd80c23a35d900c20e314b
Andrii <poltavandre@gmail.com>
2026-01-17 13:41:14 +0200

    Add: daily return

--- First.py
+++ First.py
@@ -7,6 +7,17 @@ if __name__ == "__main__":
     ticker = yf.Ticker(tSymbol)
     histData = ticker.history(period="10y")
 
-    for date, price in histData.iterrows():
-        stock.append(Stock(price["Close"], date))
+    for i in range(len(histData)):
+        date = histData.index[i].date()
+
+        if i == 0:
+            stock.append(Stock(histData["Close"][i], date))
+        else:
+            stock.append(Stock(histData["Close"][i], date))
+            stock[i].set_return_price(stock[i-1].get_price())
     
+    for i in range(5):
+        print(stock[i].get_date(), stock[i].get_price(), stock[i].get_return_price())
+    
+    for i in range(len(stock)-5, len(stock)):
+        print(stock[i].get_date(), stock[i].get_price(), stock[i].get_return_price())
--- Stock.py
+++ Stock.py
@@ -1,10 +1,19 @@
+import numpy as np
+
 class Stock:
-    def __init__(self, price=0.0, date=None):
+    def __init__(self, price=0.0, date=None, return_price=0.0):
         self.price = price
         self.date = date
+        self.return_price = return_price
 
     def get_price(self):
-        return self.price
+        return self.price   
 
     def get_date(self):
         return self.date
+
+    def set_return_price(self, prev_price):
+        self.return_price = np.log(self.price / prev_price)
+
+    def get_return_price(self):
+        return self.return_price

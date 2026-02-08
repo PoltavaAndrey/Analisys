8b606747e55d44ac7ecea2584edccffabac609e7
Andrii <poltavandre@gmail.com>
2026-02-08 21:16:23 +0200

    change: new data source

--- First.py
+++ First.py
@@ -1,24 +1,13 @@
-import yfinance as yf
+from sqlite3 import Date
 from data.Stock import Stock
 from data.MounthlyStock import MounthlyStock as MStock
 import pandas as pd
+import numpy as np
 
 if __name__ == "__main__":
-    stock = []
-    mounthly_stock = []
-    tSymbol = "AAPL"
-    ticker = yf.Ticker(tSymbol)
-    histData = ticker.history(period="1y")
 
-    for i in range(len(histData)):
-        date = histData.index[i]
-
-        if i == 0:
-            stock.append(Stock(histData["Close"][i], date))
-            histData.at[date, "return_price"] = 0.0
-        else:
-            stock.append(Stock(histData["Close"][i], date))
-            stock[i].set_return_price(stock[i-1].get_price())
-            histData.at[date, "return_price"] = stock[i].get_return_price()
-
-    # Due to issues with efinance library I have to change the data source for my project
+    df = pd.read_csv('HistoricalQuotes.csv')
+    df[' Close'] = df[' Close'].str.replace('$', '', regex=False).str.replace(',', '', regex=False).astype(float)
+    df["Return"] = np.log(df[" Close"] / df[" Close"].shift(1))
+    
+    print(df["Date"])
